acd37214db722752d3813e318724c8515e991973
cousim46 <wjdghldns24@naver.com>
2022-05-05 13:57:32 +0900

    @Component, @ComponentScan

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,13 +1,18 @@
 package hello.core.member;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
-public class MemoryMemberRepository implements MemberRepository{
-    private static Map<Long,Member> store = new HashMap<>();
+@Component
+public class MemoryMemberRepository implements MemberRepository {
+    private static Map<Long, Member> store = new HashMap<>();
+
     @Override
     public void save(Member member) {
-        store.put(member.getId(),member);
+        store.put(member.getId(), member);
     }
 
     @Override
